d9216c56bc93aab6277cf9a45e6d62558ed39fb9
Pelayo <pelayo.vieites.perez@udc.es>
2024-04-05 00:42:02 +0200

    Eliminada clase Classifier (unused).

--- src/state_classification/classifiers.py
+++ src/state_classification/classifiers.py
@@ -22,40 +22,6 @@ classifier_dict = {'AB': AdaBoostClassifier(learning_rate=0.0645050649100435, n_
                    }
 
 
-class Classifier:
-    def __init__(self, dataset):
-        self.classifier = None
-        self.x = dataset[:, 0:2]
-        self.y = dataset[:, 2]
-        self.kfold = KFold(n_splits=10, shuffle=True, random_state=0)
-        self.scores = None
-
-    def __str__(self):
-        if self.classifier is None:
-            return "No classifier set"
-        if self.scores is None:
-            return "Classifier has not been executed yet"
-        return ("Classifier: " + str(self.classifier) +
-                "\nCross-validation scores: " + str(self.scores) +
-                "\nAverage accuracy: " + str(self.scores.mean()) +
-                "\nStandard deviation: " + str(self.scores.std()))
-
-    def set_tree_classifier(self):
-        self.classifier = DecisionTreeClassifier()
-
-    def set_knn_classifier(self, n_neighbors=3):
-        self.classifier = KNeighborsClassifier(n_neighbors)
-
-    def set_svm_classifier(self, kernel='linear'):
-        self.classifier = SVC(kernel=kernel)
-
-    def classify(self):
-        if self.classifier is None:
-            print("No classifier set")
-            return
-        self.scores = cross_val_score(self.classifier, self.x, self.y, cv=self.kfold)
-
-
 class AllClassifiers:
     def __init__(self, dataset, n_folds=10):
         # Set inputs and targets of classifiers
